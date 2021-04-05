a27144fca0658bb55663a9ec1486514636aaa6d5
manturovDan <manturov.dan@mail.ru>
2021-04-05 23:56:07 +0300

    full cross3 branches copy test

--- tests/AlgorithmTests.java
+++ tests/AlgorithmTests.java
@@ -28,14 +28,92 @@ public class AlgorithmTests {
         return (HashMap<String, List<String>>) commitGraphField.get(graph);
     }
 
-    @Test
-    public void allCommits() throws Exception {
+
+    public void cross3BranchBCTest(String branchBC) throws Exception {
         HTTPGitHub interaction = new HTTPGitHub("manturovDanExperimental", "cross3", "");
         DeepFirstSearchInRepo dfs = new DeepFirstSearchInRepo(interaction);
-        dfs.buildGitGraph("B");
+        dfs.buildGitGraph(branchBC);
         SearchStorage storage = getStorageFromDFS(dfs);
         HashMap<String, List<String>> graph = getGraphFromStorage(storage);
 
         Assertions.assertEquals(graph.size(), 5);
+
+        Assertions.assertEquals(graph.get("3aab5282d567ed69daaec373e7cc5bc1985015ef").size(), 3);
+        Assertions.assertTrue(graph.get("3aab5282d567ed69daaec373e7cc5bc1985015ef")
+                .contains("615ba764d7ce72e496324b28dab880a6fed56455"));
+        Assertions.assertTrue(graph.get("3aab5282d567ed69daaec373e7cc5bc1985015ef")
+                .contains("55242422e68e4b9f817e3fe9702e2fa49859c2cf"));
+        Assertions.assertTrue(graph.get("3aab5282d567ed69daaec373e7cc5bc1985015ef")
+                .contains("d9ac181925ef186b66efa4a82ba73e88ea3bc98a"));
+
+        Assertions.assertEquals(graph.get("18944685cb9f413ee3e52cdb6db02559c6fdcccd").size(), 0);
+
+        Assertions.assertEquals(graph.get("615ba764d7ce72e496324b28dab880a6fed56455").size(), 1);
+        Assertions.assertTrue(graph.get("615ba764d7ce72e496324b28dab880a6fed56455")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+
+        Assertions.assertEquals(graph.get("55242422e68e4b9f817e3fe9702e2fa49859c2cf").size(), 1);
+        Assertions.assertTrue(graph.get("55242422e68e4b9f817e3fe9702e2fa49859c2cf")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+
+        Assertions.assertEquals(graph.get("d9ac181925ef186b66efa4a82ba73e88ea3bc98a").size(), 1);
+        Assertions.assertTrue(graph.get("d9ac181925ef186b66efa4a82ba73e88ea3bc98a")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+    }
+
+    @Test
+    public void cross3BranchBTest() throws Exception {
+        cross3BranchBCTest("B");
+    }
+
+    @Test
+    public void cross3BranchCTest() throws Exception {
+        cross3BranchBCTest("C");
+    }
+
+    @Test
+    public void cross3BranchATest() throws Exception {
+        HTTPGitHub interaction = new HTTPGitHub("manturovDanExperimental", "cross3", "");
+        DeepFirstSearchInRepo dfs = new DeepFirstSearchInRepo(interaction);
+        dfs.buildGitGraph("A");
+        SearchStorage storage = getStorageFromDFS(dfs);
+        HashMap<String, List<String>> graph = getGraphFromStorage(storage);
+
+        Assertions.assertEquals(graph.size(), 5);
+
+        Assertions.assertEquals(graph.get("84e222f75a5b37b63602abcb2b46f9984093d3d7").size(), 3);
+        Assertions.assertTrue(graph.get("84e222f75a5b37b63602abcb2b46f9984093d3d7")
+                .contains("615ba764d7ce72e496324b28dab880a6fed56455"));
+        Assertions.assertTrue(graph.get("84e222f75a5b37b63602abcb2b46f9984093d3d7")
+                .contains("55242422e68e4b9f817e3fe9702e2fa49859c2cf"));
+        Assertions.assertTrue(graph.get("84e222f75a5b37b63602abcb2b46f9984093d3d7")
+                .contains("d9ac181925ef186b66efa4a82ba73e88ea3bc98a"));
+
+        Assertions.assertEquals(graph.get("18944685cb9f413ee3e52cdb6db02559c6fdcccd").size(), 0);
+
+        Assertions.assertEquals(graph.get("615ba764d7ce72e496324b28dab880a6fed56455").size(), 1);
+        Assertions.assertTrue(graph.get("615ba764d7ce72e496324b28dab880a6fed56455")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+
+        Assertions.assertEquals(graph.get("55242422e68e4b9f817e3fe9702e2fa49859c2cf").size(), 1);
+        Assertions.assertTrue(graph.get("55242422e68e4b9f817e3fe9702e2fa49859c2cf")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+
+        Assertions.assertEquals(graph.get("d9ac181925ef186b66efa4a82ba73e88ea3bc98a").size(), 1);
+        Assertions.assertTrue(graph.get("d9ac181925ef186b66efa4a82ba73e88ea3bc98a")
+                .contains("18944685cb9f413ee3e52cdb6db02559c6fdcccd"));
+    }
+
+    @Test
+    public void cross3BranchMasterTest() throws Exception {
+        HTTPGitHub interaction = new HTTPGitHub("manturovDanExperimental", "cross3", "");
+        DeepFirstSearchInRepo dfs = new DeepFirstSearchInRepo(interaction);
+        dfs.buildGitGraph("master");
+        SearchStorage storage = getStorageFromDFS(dfs);
+        HashMap<String, List<String>> graph = getGraphFromStorage(storage);
+
+        Assertions.assertEquals(graph.size(), 1);
+
+        Assertions.assertEquals(graph.get("18944685cb9f413ee3e52cdb6db02559c6fdcccd").size(), 0);
     }
 }

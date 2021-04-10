d2b131a2764f6dcd664b780f1f18652410e0851d
manturovDan <manturov.dan@mail.ru>
2021-04-10 17:09:30 +0300

    found caching bug: 1-12 norm, then 7-10 - not norm (multiple branching)

---
+++ tests/MultipleBranching.java
@@ -0,0 +1,28 @@
+import lastCommonCommitsGitHub.finder.LastCommonCommitsFinder;
+import lastCommonCommitsGitHub.finder.LastCommonCommitsFinderFactory;
+import lastCommonCommitsGitHub.finder.LastCommonCommitsFinderFactoryGitHub;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class MultipleBranching {
+    @Test
+    public void complexRepo() throws IOException {
+        System.out.println("MultipleBranching Test");
+        LastCommonCommitsFinderFactory factory = new LastCommonCommitsFinderFactoryGitHub();
+        LastCommonCommitsFinder finder = factory.create("manturovDanExperimental", "multipleBranching", AlgorithmTests.token);
+        for (String branchesPair : MultipleBranchingMergeBase.lastCommon.keySet()) {
+            String[] branches = branchesPair.split(":");
+            System.out.println(Arrays.toString(branches));
+            Collection<String> result = finder.findLastCommonCommits(branches[0], branches[1]);
+
+            String out = result.stream().map(String::valueOf).collect(Collectors.joining(" "));
+
+            Assertions.assertEquals(MultipleBranchingMergeBase.lastCommon.get(branchesPair), out);
+        }
+    }
+}
